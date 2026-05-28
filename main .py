3b4c153b5b0c41e86ed36b721f55bb24a3b1c70e
nerd-10 <nilanshgupta103@gmail.com>
2026-05-28 23:38:33 +0530

    improved

--- main .py
+++ main .py
@@ -5,21 +5,22 @@ from action import DefendAction
 from action import StaminaRecoveryAction
 from game_engine import GameEngine
 
-hero = Character("Hero", 100, 50, [AttackAction("Slash", 10, 5), HealAction("Heal", 20, 10), DefendAction("Defend", 5), StaminaRecoveryAction("Rest",8)])
-enemy = Character("Enemy", 80, 40, [AttackAction("Bite", 6, 4), HealAction("Heal", 15, 8), DefendAction("Defend", 4), StaminaRecoveryAction("Rest",6)])
 
-engine = GameEngine(hero, enemy)
+
+
 while True:
     print("Menu:")
     print("1: Start Game")
     print("2: Exit")
     choice = input("Choose an option: ")
     if choice == '1':
+        hero = Character("Hero", 100, 50, [AttackAction("Slash", 10, 5), HealAction("Heal", 20, 10), DefendAction("Defend", 5), StaminaRecoveryAction("Rest",8)])
+        enemy = Character("Enemy", 80, 40, [AttackAction("Bite", 6, 4), HealAction("Heal", 15, 8), DefendAction("Defend", 4), StaminaRecoveryAction("Rest",6)])
+        engine = GameEngine(hero, enemy)
         engine.run()
-        break
+        
     elif choice == '2':
         print("Exiting game. Goodbye!")
-        exit()
+        break
     else:
         print("Invalid choice. Please enter 1 to start the game or 2 to exit.")
-    engine.run()    
