5b3cf08e41ee869f7cdd8579193e64c616e3bb28
nerd-10 <nilanshgupta103@gmail.com>
2026-05-28 23:32:34 +0530

    Menu and next new choice to exit the game loop

--- main .py
+++ main .py
@@ -9,4 +9,17 @@ hero = Character("Hero", 100, 50, [AttackAction("Slash", 10, 5), HealAction("Hea
 enemy = Character("Enemy", 80, 40, [AttackAction("Bite", 6, 4), HealAction("Heal", 15, 8), DefendAction("Defend", 4), StaminaRecoveryAction("Rest",6)])
 
 engine = GameEngine(hero, enemy)
-engine.run()    
+while True:
+    print("Menu:")
+    print("1: Start Game")
+    print("2: Exit")
+    choice = input("Choose an option: ")
+    if choice == '1':
+        engine.run()
+        break
+    elif choice == '2':
+        print("Exiting game. Goodbye!")
+        exit()
+    else:
+        print("Invalid choice. Please enter 1 to start the game or 2 to exit.")
+    engine.run()    
